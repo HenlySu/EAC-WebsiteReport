2ce94979e7bf147bdb660667973b5ab3387a87e1
HenlySuu <hsu31@myseneca.ca>
2023-03-22 19:47:16 -0400

    Entered picture for navigation bar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import NavBar from "@/src/components/NavBar"
-import Footer from "@/src/components/Footer"
-
 export default function Home(){
   return(
     <>
-      <h1>Testing </h1>
+      
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -5,10 +7,18 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NavBar (){
    return(
       <>
-         <Navbar bg="dark" variant="dark">
+         <Navbar className='navBar'>
             <Container>
-               <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-               <Nav className="me-auto">
+               <Navbar.Brand href="#home">
+                  <img 
+                     src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Seneca_College_logo.svg/1200px-Seneca_College_logo.svg.png' 
+                     className='logo'
+                     alt='Seneca' 
+                  />
+               </Navbar.Brand>
+               
+               
+               <Nav className="ml-auto">
                   <Nav.Link href="#home">Home</Nav.Link>
                   <Nav.Link href="#features">Features</Nav.Link>
                   <Nav.Link href="#pricing">Pricing</Nav.Link>
