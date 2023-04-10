aca9c721c2fab6044f7517dcc4d1a106eae77170
HenlySuu <hsu31@myseneca.ca>
2023-04-10 01:30:00 -0400

    Updated reference

--- pages/reference.jsx
+++ pages/reference.jsx
@@ -4,8 +4,26 @@ export default function Reference (){
    return (
       <>
          <Container>
+            <br />
+            <br />
             <h3>Reference</h3>
+            <br />
             
+            <p>
+               Energy Saver. (n.d.). Lighting Choices to Save You Money. https://www.energy.gov/energysaver/lighting-choices-save-you-money#:~:text=LEDs%20use%20up%20to%2090,longer%20than%20traditional%20incandescent%20bulbs
+            </p>
+
+            <p>
+               ETH ZURICH. (2019, April 7). How trees could help to save the climate. https://ethz.ch/en/news-and-events/eth-news/news/2019/07/how-trees-could-save-the-climate.html.
+            </p>
+
+            <p>
+               The Tree Council. (2020, April 22). Earth Day 2019: five amazing things trees do for the planet.
+            </p>
+
+            <p>
+               USDA. (2014, January 18). https://www.fs.usda.gov/features/lets-carry-values-tu-bshevat-us-every-day#:~:text=Many%20people%20are%20unaware%20that,plant%20trees%20in%20their%20communities
+            </p>
          </Container>
       </>
    )
