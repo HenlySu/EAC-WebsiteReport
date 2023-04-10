2d72b603aa9416b0986e67245286befbdf46218a
HenlySuu <hsu31@myseneca.ca>
2023-04-09 20:48:18 -0400

    update

--- pages/conclusion.jsx
+++ pages/conclusion.jsx
@@ -1,9 +1,56 @@
-import Conclusion from "@/src/components/Conclusion"
+import { Container } from 'react-bootstrap'
 
-export default function Conclusionfunc() {
+export default function Conclusion() {
    return (
       <>
-         <Conclusion />
+         <Container>
+            <br />
+            <h3>Conclusion</h3>
+            <br />
+
+            <p>
+               In summary, the first solution, LED lamps, saves more energy than incandescent 
+               lamps, reducing carbon dioxide emissions. Furthermore, in the long run, LED 
+               lights have a longer lifespan and are more cost-effective. However, LED 
+               bulbs are also significantly more expensive than incandescent bulbs, and 
+               purchasing and installing them can stretch the budget. Next is the second 
+               solution, which uses solar energy to provide energy. This is a sustainable 
+               and clean energy source, but its use cost is too expensive. The installation 
+               cost alone is staggering enough, not to mention the subsequent maintenance.
+            </p>
+            <p>
+               Moreover, considering that Toronto is not sunny, it is hard to say whether 
+               the expected effect will be achieved. The third solution has not only many 
+               benefits but also remarkable effects. The downside is that this solution 
+               requires much staffing and additional costs in the initial stage, and the 
+               website also needs to be managed by someone, all of which we need to consider. 
+               However, continuing and scaling up tree-planting efforts can be essential in 
+               mitigating greenhouse gas emissions for current and future generations. Seneca 
+               can contribute to this effort by supporting and participating in tree-planting 
+               initiatives and advocating for policies that promote afforestation and reforestation. 
+               Harnessing the full potential of trees in mitigating greenhouse gas emissions is a 
+               critical component of achieving sustainable goals at Seneca. Under consideration, 
+               the team has decided on the third option. 
+            </p>
+
+            <br />
+            <h3>Recommendation</h3>
+            <br />
+
+            <p>
+               For future steps, we will carry out publicity activities for the club, inform the 
+               students of our upcoming activities, and attract students willing to join us. Next, 
+               we will inspect the tree planting sites for suitable trees. This step will require 
+               professional pieces of advice from a botanist. We also need to have further planning 
+               for future actions involving how to judge the state of trees and control pests. Technical-wise, 
+               our website also requires an ongoing operation, which may involve volunteering or hiring a 
+               content planner, graphic designer, and technical staff. In addition, we can also do some 
+               extra work, such as getting in touch with local environmental protection organizations and 
+               asking questions about the details of our tree-planting actions. It is best to reach a 
+               cooperation with them so that it will be much easier for the team to get more professional 
+               guidance for our actions.
+            </p>
+         </Container>
       </>
    )
 }
